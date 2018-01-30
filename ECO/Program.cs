e3009d0158210d7091a782fc9d54ae878906a3d5
Alle <Algotify@gmail.com>
2018-01-30 11:24:02 +0100

    hello world

---
+++ ECO/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ECO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
