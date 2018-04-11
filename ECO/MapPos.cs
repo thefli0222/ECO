5a2f81bb7b41e07b007f14bd625faead3650f604
Arvid <algotify@gmail.com>
2018-04-11 16:47:55 +0200

    Finalized  location bazed nibbas :D:D

--- ECO/MapPos.cs
+++ ECO/MapPos.cs
@@ -29,39 +29,39 @@ namespace ECO
             }
             return -1;
         }
-
-        //TODO hitta posses för nuke
         private int nukePosition(float x, float y, float z)
         {
-            return -1;
             //A Site
-            if (x > -800 && y > -2630 && x < -130 && y < -1280)
+            if (x > 340 && y > -1000 && z > -360 && x < 970 && y < -340)
             {
                 return 1;
             }   //B Site
-            else if (x > -2700 && y > -270 && x < -1520 && y < 650 ||
-                     x > -2500 && y > 630 && x < -1930 && y < 870)
+            else if (x > 320 && y > -1340 && x < 950 && y < -270 && z < -510)
             {
                 return 2;
             }   //A T entry
-            else if (x > -130 && y > -2500 && x < 600 && y < -1280)
+            else if (x > -260 && y > -1400 && x < 860 && y < -1280 && z <-190 && z > -360)
             {
                 return 3;
             }   //B T entry
-            else if (x > -1920 && y > -830 && x < -970 && y < 650)
+            else if (x > 0 && y > -650 && x < 260 && y < -650 ||
+                     x < 900 && y > -250 && x > 370 && y < 340 && z < -480)
             {
                 return 4;
             }//A CT entry
-            else if (x < -1520 && y > -270 && x > -2390 && y < -730)
+            else if (x < 1300 && y > -500 && x > 950 && y < -300 && z > -100)
             {
                 return 5;
             }//B CT entry
-            else if (x < -2250 && y < -1060 && x > -800 && y > -2630)
+            else if (x < 290 && y < -1280 && x > 0 && y > -1700 && z < -510 ||
+                     x < 1430 && y < 1700 && x > 1000 && y > -390 && z < -510 ||
+                     x < 1220 && y < 700 && x > 780 && y > -230 && z < -510)
             {
                 return 6;
             }
             //Mid
-            else if (x < 530 && y > -950 && x > -1220 && y < -50)
+            else if (x < 340 && y > -1910 && x > -160 && y < -1460 && z > 0 ||
+                     x < 2000 && y > -2500 && x > -200 && y < -1620 && z > 0)
             {
                 return 7;
             }
@@ -176,38 +176,38 @@ namespace ECO
             return -1;
         }
 
-        //TODO find right pos for train
         private int trainPosition(float x, float y, float z)
         {
-            return -1;
             //A Site
-            if (x > -800 && y > -2630 && x < -130 && y < -1280)
+            if (x > -210 && y > -300 && x < 1000 && y < 300)
             {
                 return 1;
             }   //B Site
-            else if (x > -2700 && y > -270 && x < -1520 && y < 650 ||
-                     x > -2500 && y > 630 && x < -1930 && y < 870)
+            else if (x > -490 && y > -1630 && x < 490 && y < -930)
             {
                 return 2;
             }   //A T entry
-            else if (x > -130 && y > -2500 && x < 600 && y < -1280)
+            else if (x > -760 && y > -450 && x < -320 && y < -220 ||
+                     x > -860 && y < 750 && x < -400 && y > 380 ||
+                     x > 1150 && y < 1730 && x < 1450 && y > 650)
             {
                 return 3;
             }   //B T entry
-            else if (x > -1920 && y > -830 && x < -970 && y < 650)
+            else if (x > -1160 && y > -1770 && x < 490 && y < -500)
             {
                 return 4;
             }//A CT entry
-            else if (x < -1520 && y > -270 && x > -2390 && y < -730)
+            else if (x < 1100 && y > -810 && x > 300 && y < -280 ||
+                     x > 1010 && y < 0 && x < 1970 && y > -260)
             {
                 return 5;
             }//B CT entry
-            else if (x < -2250 && y < -1060 && x > -800 && y > -2630)
+            else if (x < 1540 && y < -1850 && x > 490 && y > -790)
             {
                 return 6;
             }
             //Mid
-            else if (x < 530 && y > -950 && x > -1220 && y < -50)
+            else if (x < 2000 && y > 300 && x > -360 && y < 600)
             {
                 return 7;
             }
